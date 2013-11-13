cc4d2812b970d911f6e097ab50f203c903c61427
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2013-11-13 12:34:08 +0100

    JavaTest DB
    Connectie maken met de Database en de layer

---
+++ DataBaseConnectie/src/DBConnectie.java
@@ -0,0 +1,16 @@
+public class DBConnectie
+{
+
+	// Connectie string naar de database toe
+	static String	Connectie	= "jdbc:mysql://databases.aii.avans.nl:3306/";
+
+	// De Naam van de database
+	static String	DataBase	= "agamfsch_db2";
+
+	// Inlognaam voor gebruiker
+	static String	DB_User		= "";
+
+	// Wachtwoord voor gebruiker
+	static String	DB_Password	= "";
+
+}
