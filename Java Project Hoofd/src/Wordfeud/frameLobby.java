7f0589baebd3cf733f7e8239b2fd92fdf26f458c
KaiHerwijn <km.herwijn@student.avans.nl>
2013-11-15 13:08:31 +0100

    Jscrollpane vullen en in frameLobby zetten

--- Java Project Hoofd/src/Wordfeud/frameLobby.java
+++ Java Project Hoofd/src/Wordfeud/frameLobby.java
@@ -1,6 +1,11 @@
 package Wordfeud;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 /**
  * 
@@ -9,10 +14,24 @@ import javax.swing.JFrame;
  */
 public class frameLobby extends JFrame
 {
+	private JScrollPane scrollCompetite;
+	private JList<String> listCompetite;
+	
 	public frameLobby()
 	{
-		setVisible(true);
+		listCompetite = new JList<String>();
+		listCompetite.add("Kai", new JLabel("Kai"));
+		
+		scrollCompetite = new JScrollPane();
+		scrollCompetite.setVisible(true);
+		scrollCompetite.add(listCompetite);
+		scrollCompetite.setSize(80, 200);
+		
+		setLayout(new BorderLayout());
+		add(scrollCompetite);
 		setTitle("wordfeud");
 		setSize(600, 400);
+		setEnabled(true);
+		setVisible(true);
 	}
 }
