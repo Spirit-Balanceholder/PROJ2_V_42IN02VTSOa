bfe3b961415679aaa2865cc8e6883138cf6aa0d8
KaiHerwijn <km.herwijn@student.avans.nl>
2013-11-15 12:22:39 +0100

    setupklasse en mainfunctie toegevoegd

---
+++ Java Project Hoofd/src/Wordfeud/frameLobby.java
@@ -0,0 +1,18 @@
+package Wordfeud;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Kai
+ * 
+ */
+public class frameLobby extends JFrame
+{
+	public frameLobby()
+	{
+		setVisible(true);
+		setTitle("wordfeud");
+		setSize(600, 400);
+	}
+}
---
+++ Java Project Hoofd/src/Wordfeud/setup.java
@@ -0,0 +1,17 @@
+package Wordfeud;
+/**
+ * 
+ * @author Kai
+ * Klasse met niet meer dan de main() voor de hele applicatie.
+ *
+ */
+
+public class setup {
+	/**
+	 * Deze functie zet alleen het spel op waarna de gebruiker zijn spellen kan beginnen.
+	 */
+	public static void main(String[] args)
+	{
+		frameLobby mainobject = new frameLobby();
+	}
+}
