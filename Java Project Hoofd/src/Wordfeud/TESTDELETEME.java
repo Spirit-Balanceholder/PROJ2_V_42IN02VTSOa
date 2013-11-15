8986d91e6b8b55b28e46a226c93c5905c7ff4595
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2013-11-15 11:19:08 +0100

    s
    s

--- Java Project Hoofd/src/Wordfeud/TESTDELETEME.java
+++ Java Project Hoofd/src/Wordfeud/TESTDELETEME.java
@@ -2,15 +2,21 @@ package Wordfeud;
 
 public class TESTDELETEME
 {
+	// <<<<<<< HEAD
 
 	public void DumpME()
 	{
 		int x = 10;
 	}
 
+	// =======
+	// >>>>>>> 125855b68cec85fbc18213c53af928900979fc33
 	public void getme()
 	{
 		System.out.print("hoi");
 	}
+	// <<<<<<< HEAD
 
+	// =======
+	// >>>>>>> 125855b68cec85fbc18213c53af928900979fc33
 }
